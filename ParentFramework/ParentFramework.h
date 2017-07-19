73f02f75421847bfe5d8082173c7cda702b5f972
saurabhverma <vermasaura@gmail.com>
2017-07-19 15:46:21 +1000

    Initial commit

---
+++ ParentFramework/ParentFramework.h
@@ -0,0 +1,19 @@
+//
+//  ParentFramework.h
+//  ParentFramework
+//
+//  Created by Saurabh Verma on 19/7/17.
+//  Copyright © 2017 Outware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParentFramework.
+FOUNDATION_EXPORT double ParentFrameworkVersionNumber;
+
+//! Project version string for ParentFramework.
+FOUNDATION_EXPORT const unsigned char ParentFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParentFramework/PublicHeader.h>
+
+
